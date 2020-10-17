087e0a44d4baa62e6d4f49827ed517d6cfb45f3c
jayaraj k <jayaraj@Altait.com>
2020-10-17 12:29:40 +0530

    initial commit

---
+++ app/src/main/java/com/jayaraj/coronastatus/User.java
@@ -0,0 +1,14 @@
+package com.jayaraj.coronastatus;
+
+public class User {
+
+  public String name;
+  public String email;
+
+  public User() {}
+
+  public User(String name, String email) {
+    this.name = name;
+    this.email = email;
+  }
+}
